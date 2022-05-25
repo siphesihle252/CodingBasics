9cc77a62d0b72744dc79d84de9945afb1318bdf7
Siphesihle Ntwanambi <59652218+siphesihle252@users.noreply.github.com>
2022-05-25 12:49:43 +0200

    Add files via upload

---
+++ style.js
@@ -0,0 +1,35 @@
+//var
+var code = 4;
+var html = code;
+document.write("The output of html is:", +html);
+//Line-break
+document.write("<br>")
+//Const
+const output = "Hi";
+const bree = output;
+document.write(bree);
+document.write("<br>")
+//Let
+let cape = "Coding";
+console.log(cape)
+document.write("<br>")
+//While Loop
+let num = 3;
+while (num <=9) {
+    document.writeln(num + "items");
+    num ++;
+}
+document.write("<br>")
+//Do...while Loop
+var count = 0;
+do {
+    document.writeln(count);
+    count ++;
+}
+while(count <=6)
+document.write("<br>")
+//For Loop
+for (var bee =2; bee <10; bee ++) {
+    document.writeln(bee);
+}
+document.writeln("The End!")
